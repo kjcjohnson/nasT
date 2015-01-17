fe3bd7c16e983489b669fe72f88d6dcec46775c8
Aidan Coyle <packrat386@gmail.com>
2015-01-17 18:47:52 -0500

    working version of the handler template

---
+++ nTemplates/handler_temp.sh
@@ -0,0 +1,30 @@
+URL="/home"
+METHOD=$1
+data_k=()
+data_v=()
+
+i=1;
+for arg in "$@" 
+do
+    if [ $i -eq 1 ] 
+    then
+	i=$((i+1))
+	continue;
+    fi
+    if [ $(($i % 2)) -eq 1 ]
+    then
+	data_v+=($arg);
+    else
+	data_k+=($arg);
+    fi
+    i=$((i+1))
+done
+
+echo "URL: $URL"
+echo "METHOD: $METHOD"
+echo "KEYS: ${data_k[*]}"
+echo "VALUES: ${data_v[*]}"
+
+### DON'T EDIT ABOVE HERE ###
+# unless you know what you're doing 
+
