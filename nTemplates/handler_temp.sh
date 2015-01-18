d488e08796f751c633eceaeafdc4b4b8f17cde4b
Keith Johnson <kjcjohnson@ymail.com>
2015-01-17 21:23:46 -0500

    Add default template

--- nTemplates/handler_temp.sh
+++ nTemplates/handler_temp.sh
@@ -25,6 +25,45 @@ echo "METHOD: $METHOD"
 echo "KEYS: ${data_k[*]}"
 echo "VALUES: ${data_v[*]}"
 
-###### DON'T EDIT ABOVE HERE ######
-# unless you know what you're doing 
+###### DON'T EDIT ABOVE HERE ########
+# unless you know what you're doing # 
+#####################################
+
+## Basic usage of this file:
+##   database calls
+##   webpage generation
+##   calling the template processor
+##
+## Automagic variables:
+##   $URL is the page url
+##   $METHOD is the request method
+##   $data_k is an array of the get/post keys
+##   $data_v is an array of the get/post values
+
+####################################
+## WHEN FINISHED
+# To output to the client, echo to standard output
+# Note that you must echo an HTTP header first.
+# For your convenience, some common header files are available
+# in ../headers/ :
+#       HTTP200OK
+#       HTTP404NOTFOUND
+#
+# To use just run
+#   cat ../headers/HTTP200OK 
+##
+####################################
+
+##Example
+##  
+
+cat ../headers/HTTP200OK
+
+echo "<html><head><title>nasT Test Page</title></head>"
+echo "<body>"
+echo "<h1>Welcome to nasT, a portable MVC webserver built only with UNIX utilities</h1>"
+echo "<p>To get started, review the documentation.</p>"
+echo "<hr/>"
+echo "<h3>This was built by Aidan Coyle and Keith Johnson in about 24 hours. Good luck.</h3>"
+echo "</body></html>"
 
